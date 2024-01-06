dda8b38246136ba0d2aca54052b81d56ed3141be
Mike Retondo <mretondo@mac.com>
2024-01-06 16:58:16 -0600

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Mike Retondo on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
